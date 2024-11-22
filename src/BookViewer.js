4ec3144f16e09c0d91d3f4063289ecce6217275f
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-11-22 08:04:17 -0800

    commenting out modal for now. just seeing if book page preview loads dynamically. remember JSX comment symbols

--- src/BookViewer.js
+++ src/BookViewer.js
@@ -2,87 +2,73 @@ import {useRef, useState, useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-
 const BookViewer = () => { 
-   const canvasRef = useRef();     
-   
-   const [loaded, setLoaded] = useState(false);  
-   
- const [show, setShow] = useState(false);
+	const canvasRef = useRef();
+	const [loaded, setLoaded] = useState(false);
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-   
-   useEffect(()=> {      
-      const scriptTag = document.createElement('script')                
-      scriptTag.src= 'https://www.google.com/books/jsapi.js'       
-      scriptTag.addEventListener('load', ()=>setLoaded(true))       
-      scriptTag.id = "google-script"      
-      document.body.appendChild(scriptTag);       
-    }, []);      
+	useEffect(()=> {
+		const scriptTag = document.createElement('script')                
+		scriptTag.src= 'https://www.google.com/books/jsapi.js'       
+		scriptTag.addEventListener('load', ()=>setLoaded(true))       
+		scriptTag.id = "google-script"      
+		document.body.appendChild(scriptTag);       
+	}, []);      
 
-   
-   useEffect(()=>{            
-       if (!loaded) return             
-       else{         
-            if(window.viewer){            
-               let viewer = new window.google.books.DefaultViewer(canvasRef.current); 
-               viewer.load('ISBN:9780394800028');                    
-             }        
-             else{          
-               
-               window.google.books.setOnLoadCallback(() => {                 
-                let viewer = new window.google.books.DefaultViewer(canvasRef.current);         
-                window.viewer = viewer         
-                window.viewer.load('ISBN:9780394800028');        
-                });
-               window.google.books.load();                             
-           }              
-       }
-   }, [loaded])      
+	useEffect(()=> {
+		if (!loaded) return             
+			else{         
+				if(window.viewer){            
+					let viewer = new window.google.books.DefaultViewer(canvasRef.current); 
+					viewer.load('ISBN:9780394800028');
+				}        
+				else{
+					window.google.books.setOnLoadCallback(() => {                 
+						let viewer = new window.google.books.DefaultViewer(canvasRef.current);         
+						window.viewer = viewer         
+						window.viewer.load('ISBN:9780394800028');
+					});
+                    window.google.books.load();
+				}
+			}
+		}, [loaded])
 
-   
- const divStyle = {
-    height: '600px',
-    width: '500px'
-  };
+					
+    const divStyle = {
+        height: '600px',
+        width: '500px'
+// display: 'none'
+    };
 
-       return (    
-    <>
-     <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-{/*          <div>        
-              {loaded ?             
-                   <div>                
-                      <div ref={canvasRef} id="viewerCanvas" style={divStyle}></div>            
-                   </div> 
-                   : 
-              'Script not loaded'}      
-          </div>   */}
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal again
+            </Button>
 
 
-<Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div ref={canvasRef} id="viewerCanvas" style={divStyle}></div>   
+{/*           <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>*/}
 
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
 
-      </> 
-      )
+            <div ref={canvasRef} id="viewerCanvas" style={divStyle}></div>
+        </>
+    )
 
 }
 
